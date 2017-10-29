5add3435dc9a28a8d40cf486f16e6fb73e664226
ff_roy <roy@firstfactory,com>
2017-10-29 17:24:33 -0600

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Header headerText={'Albums'} />
+        <AlbumList />
+      </View>
+    );
+  }
+}
